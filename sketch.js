45cdd7f5a3c7ceddb51b5b8f9de435bb8a67c947
perumrr <74992461+perumrr@users.noreply.github.com>
2025-01-27 22:30:31 +0000

    testing

--- sketch.js
+++ sketch.js
@@ -21,11 +21,14 @@ function imageLoaded() {
 
 function classifyImage(img) {
   classifier.classify(img, (err, results) => {
-    if (err) {
-      console.error('Error during classification:', err);
-      resultDiv.html('Error classifying the image. Please try again.');
-      return;
-    }
+    // if (err) {
+    //   console.error('Error during classification:', err);
+    //   resultDiv.html('Error classifying the image. Please try again.');
+    //   return;
+    // }
+    
+    resultDiv.html(results[0].label);
+    resultDiv.html(nf(results[0].confidence, 0, 2));
 
     const label = results[0].label;
     const confidence = nf(results[0].confidence, 0, 2);
