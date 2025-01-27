fa738dcf05751c1678d324aca797fcc985c61461
perumrr <74992461+perumrr@users.noreply.github.com>
2025-01-27 18:58:02 +0000

    fixing

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
 let img;
 let classifier;
+let img;
+
+function preload() {
+    classifier = ml5.imageClassifier("MobileNet");
+    img = loadImage("images/bird.png");
+}
 
 let label = "";
 let confidence = "";
@@ -9,6 +15,24 @@ function preload() {
   img = loadImage("images/word.jpg");
 }
 
+function setup() {
+    createCanvas(400, 400);
+    image(img, 0, 0, width, height); 
+    classifier.classify(img, gotResult); 
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log(results);
+  
+    fill(255);
+    textSize(18);
+    text("Label: " + results[0].label, 10, height - 40);
+    text("Confidence: " + nf(results[0].confidence, 0, 2), 10, height - 20);
+}
 function setup() {
   createCanvas(400, 400);
   classifier.classify(img, gotResult);
