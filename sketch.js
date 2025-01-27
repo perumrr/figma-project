ea329dcf415fe4fe15e4fcd1f78da655397a790e
perumrr <74992461+perumrr@users.noreply.github.com>
2025-01-27 22:35:12 +0000

    changes

--- sketch.js
+++ sketch.js
@@ -1,10 +1,6 @@
 let resultDiv;
 let img;
-const classifier = ml5.imageClassifier('MobileNet', () => {
-  console.log('Model Loaded!');
-  resultDiv.html('Classifying the image...');
-  classifyImage(img);
-});
+const classifier = ml5.imageClassifier('MobileNet', modelReady);
 
 function setup() {
   noCanvas();
@@ -15,24 +11,26 @@ function setup() {
   img = createImg('bird.png', imageLoaded);
 }
 
+function modelReady() {
+  console.log('Model Loaded!');
+  resultDiv.html('Classifying the image...');
+
+  classifyImage(img);
+}
+
 function imageLoaded() {
   console.log('Image loaded successfully.');
 }
 
 function classifyImage(img) {
   classifier.classify(img, (err, results) => {
-     if (err) {
-      console.error('Error during classification:', err);
-      resultDiv.html('Error classifying the image. Please try again.', results[0].label);
+    if (err) {
+      console.error(err);
+      resultDiv.html('Error classifying the image.');
       return;
     }
-    
-    resultDiv.html(results[0].label);
-    resultDiv.html(nf(results[0].confidence, 0, 2));
 
-    const label = results[0].label;
-    const confidence = nf(results[0].confidence, 0, 2);
-
-    resultDiv.html(`Label: ${label} <br> Confidence: ${confidence}`);
+    const resultsJSON = JSON.stringify(results, null, 2);
+    resultDiv.html(`<pre>${resultsJSON}</pre>`);
   });
 }
