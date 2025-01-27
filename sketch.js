5504aac409681718c130236a4d46fd80806e3c58
perumrr <74992461+perumrr@users.noreply.github.com>
2025-01-27 19:03:14 +0000

    updating code

--- sketch.js
+++ sketch.js
@@ -1,18 +1,9 @@
-let img;
 let classifier;
 let img;
 
 function preload() {
     classifier = ml5.imageClassifier("MobileNet");
     img = loadImage("images/bird.png");
-}
-
-let label = "";
-let confidence = "";
-
-function preload() {
-  classifier = ml5.imageClassifier("mobilenet");
-  img = loadImage("images/word.jpg");
 }
 
 function setup() {
@@ -33,25 +24,3 @@ function gotResult(error, results) {
     text("Label: " + results[0].label, 10, height - 40);
     text("Confidence: " + nf(results[0].confidence, 0, 2), 10, height - 20);
 }
-function setup() {
-  createCanvas(400, 400);
-  classifier.classify(img, gotResult);
-  image(img, 0, 0, width, height);
-}
-
-// Callback function for when classification has finished
-function gotResult(results) {
-  // The results are in an array ordered by confidence
-  console.log(results);
-  // Display the results on the canvas
-  fill(255);
-  stroke(0);
-  textSize(18);
-  
-  label = "Label: " + results[0].label;
-  confidence = "Confidence: " + nf(results[0].confidence, 0, 2);
-  
-  text(label, 10, 360);
-  text(confidence, 10, 380);
-}
-
